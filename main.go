2bd70ebc52eba84b69b748668550e82c16317a71
Grigory <grigoriy.lantsov@domrf.ru>
2024-02-21 19:10:00 +0300

    rewrite go code

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+)
 
 func handler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Hello, Grigory!")
 }
 
 func main() {
-  http.HadleFunc("/", handler)
-  fmt.Println(":8080", nil)
+  http.HandleFunc("/", handler)
+  fmt.Println("Server is listening on 8080 port")
+  http.ListenAndServe(":8080", nil)
 }
